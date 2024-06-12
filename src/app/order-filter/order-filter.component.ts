a5c48ac565850c2cf4b518090c5fe095f6b59ef3
Jakub Pištěk <mail@jakubpistek.cz>
2024-06-12 16:30:32 +0200

    Improve visuals, translate statuses

--- src/app/order-filter/order-filter.component.ts
+++ src/app/order-filter/order-filter.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { NzCheckboxComponent, NzCheckboxGroupComponent } from 'ng-zorro-antd/checkbox';
+import { NzCheckboxComponent, NzCheckboxGroupComponent, NzCheckBoxOptionInterface } from 'ng-zorro-antd/checkbox';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { NzOptionComponent, NzSelectComponent } from 'ng-zorro-antd/select';
 import { NgForOf } from '@angular/common';
@@ -7,7 +7,7 @@ import { NzDividerComponent } from 'ng-zorro-antd/divider';
 import { NzDatePickerComponent } from 'ng-zorro-antd/date-picker';
 import { NzInputDirective, NzInputGroupComponent } from 'ng-zorro-antd/input';
 import { NzIconDirective } from 'ng-zorro-antd/icon';
-import { OrdersFilter } from '../../types';
+import { OrdersFilter, OrderStatus } from '../../types';
 
 @Component({
   selector: 'ce-order-filter',
@@ -39,8 +39,8 @@ export class OrderFilterComponent {
   protected filterForm: FormGroup = new FormGroup({
     status: new FormControl(this.statusOptions),
     productLine: new FormControl('all'),
-    from: new FormControl(new Date("2022-01-01")),
-    to: new FormControl(new Date("2022-12-31")),
+    from: new FormControl(new Date('2022-01-01')),
+    to: new FormControl(new Date('2022-12-31')),
     orderNumber: new FormControl(null),
   });
 
@@ -49,9 +49,19 @@ export class OrderFilterComponent {
   @Output() filterChange: EventEmitter<OrdersFilter> = new EventEmitter();
 
   constructor() {
-    this.filterForm.valueChanges.subscribe(f => this.filterChange.emit({
-      ...f, orderNumber: +f.orderNumber
-    }));
+    // TODO eww, rewrite it nicer
+    this.filterForm.valueChanges.subscribe(filterValue => {
+      const filter: OrdersFilter = {
+        ...filterValue,
+        orderNumber: filterValue.orderNumber ? parseInt(filterValue.orderNumber) : undefined,
+        status: filterValue.status ? filterValue.status.filter((option: NzCheckBoxOptionInterface) =>
+          option.checked).map((option: NzCheckBoxOptionInterface) => option.value as OrderStatus) : [],
+        productLine: filterValue.productLine ? filterValue.productLine : undefined,
+        from: filterValue.from ? filterValue.from : undefined,
+        to: filterValue.to ? filterValue.to : undefined,
+      };
+      this.filterChange.emit(filter);
+    });
   }
 
 }
--- src/app/order-status/order-status.component.ts
+++ src/app/order-status/order-status.component.ts
@@ -5,11 +5,24 @@ import { OrderStatus } from '../../types';
   selector: 'ce-order-status',
   standalone: true,
   imports: [],
-  template: '<div [className]="status">{{ status }}</div>',
+  template: '<div [className]="status">{{ translateStatus(status) }}</div>',
   styleUrl: './order-status.component.css',
 })
 export class OrderStatusComponent {
 
   @Input() status!: OrderStatus;
 
+  //todo make scalable
+  translateStatus(status: OrderStatus): string {
+    switch (status) {
+      case OrderStatus.inProgress:
+        return 'In Progress';
+      case OrderStatus.pending:
+        return 'Pending';
+      case OrderStatus.completed:
+        return 'Completed';
+      default:
+        return status;
+    }
+  }
 }
